bd198d196582fc8069e325978382877b3e3c73b6
dangola02 <zhanataev.kostanay@gmail.com>
2022-06-28 16:47:41 +0600

    Second Task

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-///////////TASK 1
-
-
+// ///////////TASK 1
 
 const concatStrings = function (arg, separator) {
 
@@ -36,3 +34,78 @@ const concatStrings = function (arg, separator) {
 
     return concat;
 }
+
+
+/////TASK 2
+
+class Calculator {
+
+    static checkNum(num) {
+
+        if (typeof (num) !== typeof (6) || num === undefined || num === Infinity || num === -Infinity || isNaN(num) === true) {
+            throw new Error("Number is not Valid or Empty")
+        }
+    }
+
+    constructor(a, b) {
+
+        Calculator.checkNum(a);
+        Calculator.checkNum(b);
+
+        this.a = a;
+        this.b = b;
+
+    }
+
+
+    setX(num) {
+        Calculator.checkNum(num);
+
+        this.a = num;
+
+        return this;
+
+    }
+
+    setY(num) {
+        Calculator.checkNum(num);
+
+        this.b = num;
+
+        return this;
+
+    }
+
+    logSum = () => {
+        console.log(this.a + this.b);
+
+        return this;
+
+    }
+
+    logMul = () => {
+        console.log(this.a * this.b);
+
+        return this;
+    }
+
+    logSub = () => {
+        console.log(this.a - this.b);
+
+    }
+
+    logDiv = () => {
+        if (this.b === 0) {
+            throw new Error("Can't divide by 0")
+
+        }
+        console.log(this.a / this.b);
+
+        return this;
+    }
+
+}
+
+const calculator = new Calculator(0, 5);
+
+calculator.logDiv();
